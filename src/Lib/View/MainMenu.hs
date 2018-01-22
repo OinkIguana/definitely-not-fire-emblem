6f3cb70ee15ebecfdfafc5c864c6c1af3c1b5959
Cameron Eldridge <cameldridge+git@gmail.com>
2018-01-22 09:02:16 -0500

    Don't draw the menu so huge

--- src/Lib/View/MainMenu.hs
+++ src/Lib/View/MainMenu.hs
@@ -1,5 +1,6 @@
+{-# LANGUAGE NamedFieldPuns #-}
 module Lib.View.MainMenu (view) where
-  import SDL
+  import SDL (copy, queryTexture, TextureInfo(..))
   import Lib.RC
   import Lib.Model.Game
   import Lib.View.Menu
@@ -8,4 +9,5 @@ module Lib.View.MainMenu (view) where
   view menu = do
     renderer <- getRenderer
     texture <- renderMenuQuick menu
-    copy renderer texture Nothing Nothing
+    TextureInfo { textureWidth, textureHeight } <- queryTexture texture
+    copy renderer texture Nothing (Just $ toSDL $ Rectangle 0 0 textureWidth textureHeight)
